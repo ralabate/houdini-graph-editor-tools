3bc3152ac4e7a04be22d16fcf523b801a1e3a8bb
Nicholas Ralabate <32040193+ralabate@users.noreply.github.com>
2024-02-29 14:14:02 -0800

    Remove debug prints

--- set_playback_range.py
+++ set_playback_range.py
@@ -7,7 +7,6 @@ channel_editor = hou.ui.paneTabOfType(hou.paneTabType.ChannelEditor)
 # get that panetab's delicious graph editor innards
 if channel_editor == None:
     pass
-    #print("ERROR: Could not find paneTab of type ChannelEditor")
 else:
     channel_graph = channel_editor.graph()
     selected_keyframes = channel_graph.selectedKeyframes()
@@ -18,14 +17,10 @@ else:
             for key in keys:
                 if key.frame() <= first_frame:
                     first_frame = key.frame()
-                    #print("updating first: " + str(first_frame))
                 if key.frame() >= last_frame:
                     last_frame = key.frame()
-                    #print("updating last: " + str(last_frame))
-        #print("CALLING WITH: " + str(first_frame) + " " + str(last_frame))                    
         hou.playbar.setPlaybackRange(first_frame, last_frame)      
     else:
         global_first_frame = hou.playbar.frameRange()[0]
         global_last_frame = hou.playbar.frameRange()[1]
-        hou.playbar.setPlaybackRange(global_first_frame, global_last_frame)      
-        #print("ERROR: No keyframes selected setting to " + str(global_first_frame) + "," + str(global_last_frame))
+        hou.playbar.setPlaybackRange(global_first_frame, global_last_frame)
